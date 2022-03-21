b697fe65116155e75ca4e2193047423ad671c21b
sanggaasmara <sanggaasmara@gmail.com>
2022-03-21 08:45:17 +0700

    update

--- src/component/BlogPost/Post.jsx
+++ src/component/BlogPost/Post.jsx
@@ -5,13 +5,13 @@ const Post = (props) => {
         <div className="artikel">
             <div className="gambar-artikel">
                 <img src="http://placeimg.com/80/80/tech" alt="Gambar Thumbnail Artikel" />
-            </div>
+                </div> 
             <div className="konten-artikel">
                 <div className="judul-artikel">{props.judul}</div>
                 <p className="isi-artikel">{props.isi}</p>
+                <button className="btn btn-sm btn-warning" onClick={() => props.hapusArtikel(props.idArtikel)}>Hapus</button>
             </div>
         </div>
     )
-
 }
 export default Post;
--- src/index.js
+++ src/index.js
@@ -20,10 +20,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
-import BlogPost from "./container/BlogPost/BlogPost";
+import BlogPost from "./container/BlogPost/BlogPostMahasiswa";
 
 ReactDOM.render( <BlogPost />, document.getElementById('content'));
 
-serviceWorker.unregister();
+reportWebVitals();
